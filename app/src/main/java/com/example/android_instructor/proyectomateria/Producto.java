2cc2054b5766ac46d1a66ec15a597f6ab2d205f3
Andres Vasquez <andres.vasquez.a@hotmail.com>
2017-10-13 20:55:23 -0400

    Proyecto base

---
+++ app/src/main/java/com/example/android_instructor/proyectomateria/Producto.java
@@ -0,0 +1,86 @@
+package com.example.android_instructor.proyectomateria;
+
+import java.util.Date;
+
+/**
+ * Created by Android-Instructor on 13/10/2017.
+ */
+
+public class Producto {
+
+    private int id;
+
+    //TODO cambiar a String u otro la imagen
+    private int imagen; // Resource
+    private String nombre;
+    private double precio;
+
+    private String categoria;
+    private boolean disponible;
+    private int stock;
+    private Date fechaElaboracion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Date getFechaElaboracion() {
+        return fechaElaboracion;
+    }
+
+    public void setFechaElaboracion(Date fechaElaboracion) {
+        this.fechaElaboracion = fechaElaboracion;
+    }
+}
